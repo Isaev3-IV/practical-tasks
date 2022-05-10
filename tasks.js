b68d59b813a3fd870004cd6e06cdfa868ba7608f
Liam <backgroundpositioncenter@gmail.com>
2022-05-10 21:27:17 +0300

    Fixed

--- tasks.js
+++ tasks.js
@@ -2,23 +2,31 @@
 
 //Classic->
 function toInitial(names) {
-  let separateArr = names.split(" ");
   let str = "";
-
-  for (let i = 0; i < separateArr.length; i++) {
-    str += `${separateArr[i].slice(0, 1)}. `;
+  for (let i = 0; i < names.length; i++) {
+    if (names[i] === " ") {
+      str = `${names[0]}. ${names[i + 1]}.`.toUpperCase();
+    }
   }
-  return str.toUpperCase();
+  return str;
 }
 
 // ES6
 function toInitial(names) {
   let separateArr = names.split(" ");
+  let str = "";
 
-  return separateArr
-    .map((elem) => `${elem.slice(0, 1)}.`)
-    .join(" ")
-    .toUpperCase("");
+  for (let item of separateArr) {
+    str += `${item[0].toUpperCase()}. `;
+  }
+  return str;
+}
+
+//-------------------
+function toInitial(names) {
+  let separateArr = names.split(" ");
+
+  return separateArr.map((elem) => `${elem[0].toUpperCase()}.`).join(" ");
 }
 
 console.log(toInitial("Tsoy Sergey"));
