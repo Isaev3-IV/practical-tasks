1def8c0b5fa41ee7e937bb376c61446d2118f003
Liam <backgroundpositioncenter@gmail.com>
2022-06-28 21:39:34 +0300

    Исправил условие

--- palindrom.js
+++ palindrom.js
@@ -1,5 +1,5 @@
 function polindrom(str) {
-  for (let i = str.length - 1, j = 0; i <= 0; i--, j++) {
+  for (let i = str.length - 1, j = 0; i >= 0; i--, j++) {
     if (str[i] !== str[j]) {
       return "It's not polindrom";
     }
@@ -8,3 +8,5 @@ function polindrom(str) {
 }
 
 console.log(polindrom("101"));
+console.log(polindrom("топот"));
+console.log(polindrom("топотор"));
