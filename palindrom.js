1e26838f0afce3f4c11b1a430d254798bbe899b3
Liam <backgroundpositioncenter@gmail.com>
2022-06-28 21:19:36 +0300

    Функция решения Палиндрома

---
+++ palindrom.js
@@ -0,0 +1,10 @@
+function polindrom(str) {
+  for (let i = str.length - 1, j = 0; i <= 0; i--, j++) {
+    if (str[i] !== str[j]) {
+      return "It's not polindrom";
+    }
+  }
+  return true;
+}
+
+console.log(polindrom("101"));
